f49c3315837cff873b98e9b0a6093b027fd93a8c
Brandon James Guerin <boomersooner12345@gmail.com>
2021-07-20 12:15:17 -0400

    Begin implementation of BibleFragment.kt and populate bible passage selector form

--- app/src/main/java/com/confessionsearchapptest/release1/ui/bible/BibleFragment.kt
+++ app/src/main/java/com/confessionsearchapptest/release1/ui/bible/BibleFragment.kt
@@ -1,4 +1,69 @@
 package com.confessionsearchapptest.release1.ui.bible
 
-class BibleFragment {
+import android.database.sqlite.SQLiteDatabase
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.confessionsearchapptest.release1.R
+import com.confessionsearchapptest.release1.data.documents.documentDBClassHelper
+import com.confessionsearchapptest.release1.databinding.FragmentBibleFormBinding
+
+class BibleFragment : Fragment() {
+
+    private var _binding: FragmentBibleFormBinding? = null
+    private lateinit var bibleViewModel: BibleViewModel
+var documentDB : SQLiteDatabase? = null
+    var docDBhelper : documentDBClassHelper?= null
+    var bibleTransList : ArrayList<String?> = ArrayList()
+    var bibleTranslation =""
+    var bibleBooksList : ArrayList<String?> = ArrayList()
+    var bibleChapterList : ArrayList<Int?> = ArrayList()
+    var bibleVerseNumList : ArrayList<Int?> = ArrayList()
+    var bibleSelectorSpinner : Spinner? = null
+var bibleSelectorAdapter : ArrayAdapter<String>? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBibleFormBinding.inflate(inflater, container, false)
+        bibleViewModel = ViewModelProvider(this).get(BibleViewModel::class.java)
+        docDBhelper = documentDBClassHelper(super.getContext())
+        documentDB=docDBhelper!!.readableDatabase
+
+      bibleViewModel.loadTranslations(docDBhelper!!.getAllBibleTranslations(documentDB!!))
+bibleTransList = bibleViewModel.getTranslations()
+       val root =binding.root
+        bibleSelectorSpinner = root.findViewById(R.id.bibleTranslationSelector)
+        bibleSelectorAdapter = ArrayAdapter(requireContext(),
+            R.layout.support_simple_spinner_dropdown_item,bibleTransList)
+        bibleSelectorSpinner!!.adapter = bibleSelectorAdapter
+        bibleSelectorSpinner!!.onItemSelectedListener=bibleSelectorSpinnerListener
+
+
+
+        return root
+
+    }
+
+
+    var bibleSelectorSpinnerListener: AdapterView.OnItemSelectedListener = object:
+    AdapterView.OnItemSelectedListener{
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            bibleTranslation = String.format("%s",parent!!.selectedItem.toString())
+                    }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            bibleTranslation= String.format("%s",parent!!.selectedItem.toString())
+        }
+    }
+
 }
--- app/src/main/java/com/confessionsearchapptest/release1/ui/bible/BibleViewModel.kt
+++ app/src/main/java/com/confessionsearchapptest/release1/ui/bible/BibleViewModel.kt
@@ -1,4 +1,62 @@
 package com.confessionsearchapptest.release1.ui.bible
 
-class BibleViewModel {
+import androidx.lifecycle.ViewModel
+import com.confessionsearchapptest.release1.data.bible.BibleContentsList
+import com.confessionsearchapptest.release1.data.bible.BibleTranslation
+
+class BibleViewModel : ViewModel() {
+    private var bibleBookList: ArrayList<String?> = ArrayList()
+    private var bibleTranslationList: ArrayList<String?> = ArrayList()
+
+    private var bibleChapterList: ArrayList<Int?> = ArrayList()
+    private var bibleVerseNumList: ArrayList<Int?> = ArrayList()
+    private var bibleVerseList: ArrayList<String?> = ArrayList()
+
+    fun getBooks(): ArrayList<String?> {
+        return bibleBookList
+    }
+
+    fun loadBooks(list: BibleContentsList) {
+        for (book in list)
+            bibleBookList.add(book.BookName!!)
+    }
+
+    fun getChapters(): ArrayList<Int?> {
+        return bibleChapterList
+    }
+
+    fun loadChapters(list: BibleContentsList) {
+        for (chapter in list)
+            bibleChapterList.add(chapter.ChapterNum!!)
+    }
+
+    fun getVerseNumbers(): ArrayList<Int?> {
+        return bibleVerseNumList
+    }
+
+    fun loadVerseNumbers(list: BibleContentsList) {
+        for (verseNum in list)
+            bibleVerseNumList.add(verseNum.VerseNumber!!)
+    }
+
+    fun getVerses(): ArrayList<String?> {
+        return bibleVerseList
+    }
+
+    fun loadVerses(list: BibleContentsList) {
+        for (verse in list)
+            bibleVerseList.add(verse.VerseText)
+    }
+
+    fun getTranslations(): ArrayList<String?> {
+        return bibleTranslationList
+    }
+
+    fun loadTranslations(list: ArrayList<BibleTranslation>)
+    {
+        for(translation in list)
+            bibleTranslationList.add(translation.bibleTranslationName)
+    }
+
+
 }
