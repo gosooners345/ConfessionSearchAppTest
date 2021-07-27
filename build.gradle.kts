d882f8fb3ee575170f405a8871fcef02feb88b84
Brandon James Guerin <boomersooner12345@gmail.com>
2021-07-27 13:01:13 -0400

    Update Kotlin things

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,20 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta03")
+        classpath("com.android.tools.build:gradle:7.0.0-rc01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
     }
+}
 
-
+allprojects{
+    repositories{google()
+        maven{url = uri("https:jitpack.io")}
+    }
+}
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
-}}
+}
