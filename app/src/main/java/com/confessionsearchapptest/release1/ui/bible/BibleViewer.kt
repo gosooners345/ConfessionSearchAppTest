b59daa7638d79826df54ff030119311588f75623
gosooners345 <Boomersooner12345@gmail.com>
2021-08-07 20:31:02 -0400

    Update BibleViewer.kt

--- app/src/main/java/com/confessionsearchapptest/release1/ui/bible/BibleViewer.kt
+++ app/src/main/java/com/confessionsearchapptest/release1/ui/bible/BibleViewer.kt
@@ -7,14 +7,14 @@ import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
 import androidx.fragment.app.Fragment
 
-//class BibleViewer (fm : FragmentManager,bible:BibleContents,bookName : String) : FragmentStatePagerAdapter(fm!!) 
-//{
-  
-//}
+class BibleViewer (fm : FragmentManager,bible:BibleContents,bookName : String) : FragmentStatePagerAdapter(fm!!) 
+{
+ 
+}
 
-//class BibleFragmentActivity : AppCompatActivity(){
-//var adapter : BibleViewer? = null
- // var shareList = String? = null
-  //var bibleList = BibleContentsList()
+class BibleFragmentActivity : AppCompatActivity(){
+var adapter : BibleViewer? = null
+ var shareList = String? = null
+//var bibleList = BibleContentsList()
   
-//}
+}
