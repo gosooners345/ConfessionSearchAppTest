e0acc656fc9fd2b1dbf5cdd971586e22cdc5d16d
gosooners345 <Boomersooner12345@gmail.com>
2021-08-07 22:18:51 -0400

    Update BibleFragment.kt

--- app/src/main/java/com/confessionsearchapptest/release1/ui/bible/BibleFragment.kt
+++ app/src/main/java/com/confessionsearchapptest/release1/ui/bible/BibleFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.Toast
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.view.Gravity
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
